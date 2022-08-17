f210c9260e33d088d3fbb34ee47223d6c9839fdd
ManuelPuchner <75803474+ManuelPuchner@users.noreply.github.com>
2022-08-17 20:14:25 +0200

    added tailwind + removed default stuff

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.ts;
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
