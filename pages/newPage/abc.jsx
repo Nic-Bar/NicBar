b35b04c01bf1d05811204953a4f342fcfe2a27c1
LukMayr <l.mayrhuber@students.htl-leonding.ac.at>
2022-09-02 19:06:20 +0200

    swap anime.js to framer motion

--- pages/newPage/abc.jsx
+++ pages/newPage/abc.jsx
@@ -1,10 +1,39 @@
-import anime from "animejs";
+import { AnimatePresence, motion, useTime, useTransform, useMotionValue} from "framer-motion"
 
 function abc() {
+  const time = useTime();
+  const x = useMotionValue(2)
+    const rotate = useTransform(
+      time,
+      [0, 4000], // For every 4 seconds...
+      [0, 360], // ...rotate 360deg
+      { clamp: false }
+    )
     
   return (
-    <div className="h-36 w-36 bg-darkBlue rounded-md m-6"></div>
+    <div className="min-h-full min-w-full place-content-center">
+      <AnimatePresence>
+      
+      <motion.div className="h-36 w-36 bg-darkBlue rounded-md m-6" style={{x, rotate}}
+        initial={
+          {
+            
+          }
+        }
+        animate={
+          {
+            duration: 2,
+            opacity: 1,
+            background: "#000000"
+          }
+        }
+        exit={{ opacity: 0 }}
+      />
+
+      </AnimatePresence>
+    </div>
   )
+  
 }
 
 export default abc
