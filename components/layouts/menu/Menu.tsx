dc0e156cf5af2d1132b157f62e9c74ef500edc5e
ManuelPuchner <75803474+ManuelPuchner@users.noreply.github.com>
2022-09-10 22:54:22 +0200

    fixed menu

--- components/layouts/menu/Menu.tsx
+++ components/layouts/menu/Menu.tsx
@@ -3,16 +3,12 @@ import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
 
 
-let width = 0, height = 0;
 
-if (typeof window !== "undefined") {
-  width = window.innerWidth;
-  height = window.innerHeight;
-}
 
 function Menu() {
   const router = useRouter();
   const [isMenuOpened, setIsMenuOpened] = useState(false)
+
   function toggleMenu(event:any, info:any){
     
     router.push({
@@ -28,39 +24,33 @@ function Menu() {
     }
     
   }, [])
-
-  console.log(width);
   
 
   return (
-    <div className=" fixed -bottom-2" >
+    <motion.div
+      className="bg-darkBlue z-40 h-screen w-screen rounded-md fixed left-0"
+      animate={{
+        top: !isMenuOpened ? "90vh" : "7rem",
+      }}
+    >
       <div className="flex justify-center w-screen ">
         <div>
-          <div className="flex justify-center">
-          <motion.div 
-            className="rounded-full h-36 w-36 z-20 translate-y-20" 
-            animate={
-              {background:isMenuOpened ? "#f2695c" : "#3e4759"}
-            }
+          <div
+            className="bg-darkBlue z-40 h-screen w-screen rounded-md absolute left-0"
             
-            onTap={toggleMenu}
-          >          
-          </motion.div>
-          </div>
-          <motion.div 
-            className="bg-darkBlue z-10 top-1" 
-            animate={{
-              height: isMenuOpened ? height-90 : 50,
-              width: width,
-              borderRadius: 10
-            }}
           >
-            
-          </motion.div>
+            <div className="flex justify-center">
+              <motion.div
+                className="rounded-full h-36 w-36 z-30 absolute -top-[4.5rem]"
+                animate={{ background: isMenuOpened ? "#f2695c" : "#3e4759" }}
+                onTap={toggleMenu}
+              ></motion.div>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  )
+    </motion.div>
+  );
 }
 
 export default Menu
