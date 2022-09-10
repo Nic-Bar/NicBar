a86fcca3b6805de4e04150c30a84e2d91824e773
ManuelPuchner <75803474+ManuelPuchner@users.noreply.github.com>
2022-09-10 17:48:05 +0200

    fetch testing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,27 @@
 import type { NextPage } from 'next'
 import { useQuery } from 'react-query'
 
+async function fetchConsumables() {
+  const res = await fetch("/api/consumable")
+  return res.json()
+}
 const Home: NextPage = () => {
+  const { data, isLoading } = useQuery('users', fetchConsumables);
 
   return (
     <div className="">
       <div className="flex">
         <div>
-          <h1 className='font-bold text-6xl'>Die wahrscheinlich coolste Kellerbar in Linz</h1>
+          <h1 className="font-bold text-6xl">
+            Die wahrscheinlich coolste Kellerbar in Linz
+          </h1>
         </div>
         <div>
-
+          <div>{!isLoading && <pre>{JSON.stringify(data, null, 2)}</pre>}</div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default Home
